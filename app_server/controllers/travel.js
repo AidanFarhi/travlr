902cc3b25153b631e3d412913080af229e9ff5ce
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-07-16 17:19:25 -0400

    updated travels page to fetch data from JSON file

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+const fs = require('fs')
+
+// get the trips data from the JSON file
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'))
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', {'title': 'Travlr Getaways'})
+    res.render('travel', {'title': 'Travlr Getaways', 'trips': trips})
 }
 
 module.exports = {
